d6d22bac497849c65d9e5fd6414652004def856b
noamhak <noamhak2003@gmail.com>
2024-07-29 14:38:56 +0300

    humanAgentFinished
    kind of working, needs to organize code and add more features for comfort , also slow keys speed

--- src/agents/humanAgent.py
+++ src/agents/humanAgent.py
@@ -11,31 +11,41 @@ def play(env):
     size = env.env.env.BOARD_LENGTH
     block_chosen = 0
     place_block = False
+    prev_key = ''
     while not terminated:
         place_block = False
+        temp_env = env.env.env
         next_env = env
-        if keyboard.read_key() == 'a':
+        key = keyboard.read_key()
+        if prev_key == key:
+            pass
+        elif key == 'a':
             cur_pos[0] = max(0, cur_pos[0] - 1)
-        if keyboard.read_key() == 'w':
+        elif key == 'w':
             cur_pos[1] = max(0, cur_pos[1] - 1)
-        if keyboard.read_key() == 's':
-            cur_pos[1] = min(size, cur_pos[1] + 1)
-        if keyboard.read_key() == 'd':
-            cur_pos[0] = min(size, cur_pos[0] + 1)
-        if keyboard.read_key() == 'r':
+        elif key == 's':
+            cur_pos[1] = min(size - 1, cur_pos[1] + 1)
+        elif key == 'd':
+            cur_pos[0] = min(size - 1, cur_pos[0] + 1)
+        elif key == 'r':
             block_chosen = 0
-        if keyboard.read_key() == 't':
+        elif key == 't':
             block_chosen = 1
-        if keyboard.read_key() == 'y':
+        elif key == 'y':
             block_chosen = 2
-        if keyboard.read_key() == 'j':
+        elif key == 'j':
             place_block = True
-        action = block_chosen * pow(size, 2) + cur_pos[0] * size + cur_pos[1]
-        if keyboard.read_key() == 'p':
+        action = block_chosen * pow(size, 2) + cur_pos[1] * size + cur_pos[0]
+        if key == 'p':
             print(action)
         if not place_block:
-            next_env.step(action)
-            next_env.render()
+            if next_env.env.env._board[cur_pos[1]][cur_pos[0]] == 1:
+                next_env.render()
+            else:
+                next_env.env.env._board[cur_pos[1]][cur_pos[0]] = 1
+                next_env.render()
+                next_env.env.env._board[cur_pos[1]][cur_pos[0]] = 0
+            env.env.env = temp_env
         else:
             obs, reward, terminated, _, info = env.step(action)
             states.append(
