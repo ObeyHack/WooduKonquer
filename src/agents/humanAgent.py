1ec2a95a1791c15e4b6f92e0d0b58005bbfa257d
noamhak <noamhak2003@gmail.com>
2024-07-29 13:21:09 +0300

    humanAgentForNow
    not working though

--- src/agents/humanAgent.py
+++ src/agents/humanAgent.py
@@ -1,3 +1,5 @@
+import copy
+
 import keyboard
 import gymnasium.utils.play
 
@@ -10,6 +12,7 @@ def play(env):
     block_chosen = 0
     place_block = False
     while not terminated:
+        place_block = False
         next_env = env
         if keyboard.read_key() == 'a':
             cur_pos[0] = max(0, cur_pos[0] - 1)
@@ -19,17 +22,19 @@ def play(env):
             cur_pos[1] = min(size, cur_pos[1] + 1)
         if keyboard.read_key() == 'd':
             cur_pos[0] = min(size, cur_pos[0] + 1)
-        if keyboard.read_key() == '1':
+        if keyboard.read_key() == 'r':
             block_chosen = 0
-        if keyboard.read_key() == '2':
+        if keyboard.read_key() == 't':
             block_chosen = 1
-        if keyboard.read_key() == '3':
+        if keyboard.read_key() == 'y':
             block_chosen = 2
         if keyboard.read_key() == 'j':
             place_block = True
-        action = block_chosen * pow(size, 2) + cur_pos[0] + cur_pos[1] * size
+        action = block_chosen * pow(size, 2) + cur_pos[0] * size + cur_pos[1]
+        if keyboard.read_key() == 'p':
+            print(action)
         if not place_block:
-            next_obs, next_reward, next_terminated, _, next_info = next_env.step(action)
+            next_env.step(action)
             next_env.render()
         else:
             obs, reward, terminated, _, info = env.step(action)
@@ -50,3 +55,13 @@ def play(env):
     x = 1
 
     env.close()
+
+
+# def play(env):
+#     keys2actions = {
+#         'w': 0,
+#         'a': 1,
+#         's': 2,
+#         'd': 3
+#     }
+#     gymnasium.utils.play.play(env, keys_to_action=keys2actions)
