a2db5af67988828322504ea6897118646410db23
MrObay <Sean.Cherny.Israel13@gmail.com>
2024-07-29 11:11:58 +0300

    back to human render

--- src/startGame.py
+++ src/startGame.py
@@ -3,11 +3,11 @@ import gymnasium as gym
 
 if __name__ == "__main__":
     # rgb_array for video, human for us
-    env = gym.make('gym_woodoku/Woodoku-v0', game_mode='woodoku', render_mode='rgb_array')
-    env = gym.wrappers.RecordVideo(env, video_folder='./video_folder')
+    env = gym.make('gym_woodoku/Woodoku-v0', game_mode='woodoku', render_mode='human')
+    # env = gym.wrappers.RecordVideo(env, video_folder='./video_folder')
 
     observation, info = env.reset()
-    for i in range(5):
+    for i in range(5000):
         # here we need to place the action choice
         action = env.action_space.sample()
         obs, reward, terminated, _, info = env.step(action)
