4949225468cc12c34e329ad2993cb6346563e3b3
MrObay <Sean.Cherny.Israel13@gmail.com>
2024-08-03 18:47:47 +0300

    not working maybe not needed

---
+++ src/agents/RL_agents/monteCarlo.py
@@ -0,0 +1,45 @@
+import collections
+from random import random
+
+from src import util
+from src.RLgame import RLAgent, RLgameState
+
+
+class MonteCarloAgent(RLAgent):
+    def __init__(self, **args):
+        super().__init__(**args)
+        self.q_values = collections.defaultdict(float)
+
+    def get_q_value(self, state: RLgameState, action):
+        # convert state to tuple
+        return self.q_values[state, action]
+
+    def get_value(self, state: RLgameState):
+        if state.is_terminated():
+            return 0.0
+
+        possible_actions = state.get_legal_actions()
+        return max([self.q_values[state, action] for action in possible_actions])
+
+    def get_policy(self, state: RLgameState):
+        if state.is_terminated():
+            return 0
+
+        possible_actions = state.get_legal_actions()
+        q_values = {action: self.q_values[state, action] for action in possible_actions}
+        return max(q_values, key=q_values.get)
+
+    def get_action(self, state: RLgameState):
+        if random.random() < self.epsilon:
+            action = random.choice(state.get_legal_actions())
+            return action
+
+        action = self.get_policy(state)
+        return action
+
+    def update(self, state: RLgameState, action, next_state: RLgameState, reward: int):
+        q_value = self.get_q_value(state, action)
+        next_value = self.get_value(next_state)
+        sample = reward + self.discount * next_value
+        self.q_values[state, action] = (1 - self.alpha) * q_value + self.alpha * sample
+        return self.q_values[state, action]
