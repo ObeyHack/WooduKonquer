a8c112cf75a6c2c6aab5ee81fb9731f2af557096
noamhak <noamhak2003@gmail.com>
2024-08-12 21:07:16 +0300

    features

--- src/agents/RL_agents/featureExtractor.py
+++ src/agents/RL_agents/featureExtractor.py
@@ -46,3 +46,77 @@ class SmartExtractor(FeatureExtractor):
     features["cell diff"] = np.sum(state2.board == 0) - np.sum(state.board == 0)
     features.normalize()
     return features
+
+
+class EstimationExtractor(FeatureExtractor):
+    def getFeatures(self, state, action):
+        features = util.Counter()
+        successor_game_state = state.generate_successor(action=action)
+        block, row, column = action // 81, (action % 81) // 9, (action % 81) % 9
+
+
+        # interpret an action as choosing a 3X3 square to place the block in rather then a specific location
+        square = (row // 3, column // 3)
+        square_center = (square[0] * 3 + 1, square[1] * 3 + 1)
+        block_1 = successor_game_state.block1
+        block_2 = successor_game_state.block2
+        block_3 = successor_game_state.block3
+        blocks = [block_1, block_2, block_3]
+        block_shape = blocks[block]
+        block_list = state._env.env.env._block_list
+        block_num = 0
+        for i in range(len(block_list)):
+            if np.all(np.equal(block_list[i], block_shape)):
+                block_num = i
+                break
+        # interpret board as squares with the number of bricks inside each square
+        prev_board = state.board
+        board = successor_game_state.board
+        compressed_board = board[square_center[0] - 1: square_center[0] + 2, square_center[1] - 1: square_center[1] + 2]
+        compressed_board_rep = 0
+        prev_compressed_board = prev_board[square_center[0] - 1: square_center[0] + 2, square_center[1] - 1: square_center[1] + 2]
+        coord = (row % 3, column % 3)
+        for i in range(3):
+            for j in range(3):
+                compressed_board_rep += int(compressed_board[i, j]) * (10 ** (i * 3 + j))
+        features[(compressed_board_rep, coord, block_num)] = 10 * np.max(np.sum(compressed_board == 0) - np.sum(prev_compressed_board == 0), 0)
+        features.normalize()
+        return features
+
+
+class EstimationDifExtractor(FeatureExtractor):
+    def getFeatures(self, state, action):
+        features = util.Counter()
+        successor_game_state = state.generate_successor(action=action)
+        block, row, column = action // 81, (action % 81) // 9, (action % 81) % 9
+
+
+        # interpret an action as choosing a 3X3 square to place the block in rather then a specific location
+        square = (row // 3, column // 3)
+        block_1 = successor_game_state.block1
+        block_2 = successor_game_state.block2
+        block_3 = successor_game_state.block3
+        blocks = [block_1, block_2, block_3]
+        block_shape = blocks[block]
+        block_list = state._env.env.env._block_list
+        block_num = 0
+        for i in range(len(block_list)):
+            if np.all(np.equal(block_list[i], block_shape)):
+                block_num = i
+                break
+        # interpret board as squares with the number of bricks inside each square
+        prev_board = state.board
+        board = successor_game_state.board
+        compressed_board = np.zeros(shape=(3, 3))
+        for i in range(3):
+            i_center = i * 3 + 1
+            for j in range(3):
+                j_center = j * 3 + 1
+                compressed_board[i, j] = np.sum(board[i_center - 1: i_center + 2, j_center - 1: j_center + 2])
+        compressed_board_rep = 0
+        for i in range(3):
+            for j in range(3):
+                compressed_board_rep += compressed_board[i, j] * (10 ** (i * 3 + j))
+        features[(compressed_board_rep, square, block_num)] = (10 * len(successor_game_state.get_legal_actions()) + compressed_board[square[0], square[1]] ** 2)
+        features.normalize()
+        return features
