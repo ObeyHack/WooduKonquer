eef77217d1d0b5dc12996c3669cb18c3e90e3023
MrObay <Sean.Cherny.Israel13@gmail.com>
2024-08-14 18:02:10 +0300

    Update plots.py

--- src/report/plots.py
+++ src/report/plots.py
@@ -11,10 +11,16 @@ def plot(scores, names):
     stds = [np.std(score) for score in scores]
 
     plt.figure()
-    plt.errorbar(names, means, yerr=stds, fmt='o')
+    plt.errorbar(names, means, yerr=stds, marker='o', linestyle='None')
     plt.ylabel('Score')
     plt.xlabel('Algorithm')
     plt.title('Comparison of different algorithms (mean and std)')
+
+    # add y=k line with name 'RL'
+    plt.axhline(y=3943, color='r', linestyle='--', label='RL - gym-woodoku')
+    plt.axhline(y=915, color='g', linestyle='--', label='DFS - WoodokuAI')
+    plt.axhline(y=2000, color='b', linestyle='--', label='Heuristics - WoodokuSolver')
+    plt.legend(loc="upper left")
 
 
 def prev_work():
@@ -25,6 +31,7 @@ def prev_work():
     # DFS - WoodokuAI - https://github.com/CosmicSubspace/WoodokuAI/tree/master
     score1 = np.array([555, 115, 145, 1957, 1218, 72, 1227, 2032])
     name1 = 'DFS'
+    print(np.mean(score1))
 
     # RL - https://github.com/helpingstar/gym-woodoku/tree/main
     score2 = np.array([3943])
@@ -44,7 +51,7 @@ def results():
     name1 = 'Reflex'
 
     # Minimax
-    score2 = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    score2 = np.array([2030, 647, 1231, 674, 2755])
     name2 = 'Minimax'
 
     plot([score0, score1, score2], [name0, name1, name2])
