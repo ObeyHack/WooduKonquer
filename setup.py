cb7993d272ef2792c4f5883ad8cbf5c1fb1df85a
MrObay <Sean.Cherny.Israel13@gmail.com>
2024-09-10 12:34:28 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
-
-
 from setuptools import setup, find_packages
 
 setup(
-    name="WooduKonquer",
+    name="woodokuMind",
     version="0.1.0",
-    author="helpingstar",
-    author_email="iamhelpingstar@gmail.com",
-    description = "Search and Reinforcement Learning algorithms for the game of Woodchuck",
+    author="NML",
+    description = "Search and Reinforcement Learning algorithms for the game of WooduK",
     license="MIT License",
     packages=find_packages(),
     install_requires=[
         "git+https://github.com/helpingstar/gym-woodoku.git",
+        "numpy",
+        "tqdm",
+        "keyboard",
+        "python-dotenv",
+        "seaborn",
+        "matplotlib",
+        "neptune",
+        "opencv-python",
     ],
-    python_requires=">=3.9",
+    python_requires=">=3.10",
 )
